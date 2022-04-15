d3f03f6d7111cdd87a37c38f23924649b3ddf8d7
TakeoYakura <92492416+TakeoYakura@users.noreply.github.com>
2022-04-15 11:29:56 -0700

    setup state

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const URL = 'http://localhost:9000/api/result'
+
+const initialValues = {
+  x: 2,
+  y: 2,
+  steps: 0,
+  email: '',
+  grid: [
+    [ 1, 1, false, null],
+    [ 2, 1, false, null],
+    [ 3, 1, false, null],
+    [ 1, 2, false, null],
+    [ 2, 2, false, 'B'],
+    [ 3, 2, false, null],
+    [ 1, 3, false, null],
+    [ 2, 3, false, null],
+    [ 3, 3, false, null],
+  ],
+  message: '',
+}
+
+
 
 export default function AppFunctional(props) {
+  const [x, setX] = useState(initialValues.x)
+  const [y, setY] = useState(initialValues.y)
+  const [steps, setSteps] = useState(initialValues.steps)
+  const [email, setEmail] = useState(initialValues.email)
+  const [message, setMessage] = useState(initialValues.message)
+  const [grid, setGrid] = useState(initialValues.grid)
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
